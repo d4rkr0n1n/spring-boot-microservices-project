6b65dd01e268648b4aec246ecb98ad31b535f53c
Mridul Roy <midlry.mr@gmail.com>
2023-10-11 23:29:54 +0530

    first commit

---
+++ src/main/java/com/d4rkr0n1n/springbootmicroservicesproject/SpringBootMicroservicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.springbootmicroservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d4rkr0n1n/springbootmicroservicesproject/SpringBootMicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.springbootmicroservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
